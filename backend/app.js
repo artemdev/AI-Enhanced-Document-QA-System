42a7afed120afd293edb9097b38e30be696c1f8b
Artem <artem.zimovets@gmail.com>
2025-02-08 15:27:54 +0100

    Configure docker-compose to run locally, gcloud to run remotelly.

--- backend/app.js
+++ backend/app.js
@@ -179,7 +179,7 @@ app.use((req, res, next) => {
 })
 
 // Start the server
-const PORT = process.env.PORT || 4000
+const PORT = parseInt(process.env.PORT) || 8080
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
